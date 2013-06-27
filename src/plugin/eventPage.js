1bad75267187d0379176c8fff809837b6b8ef008
michi3 <dr.michael-h@gmx.de>
2013-06-27 23:07:53 +0200

    falls plugin erkennt, root-tag == OAI-PMH -> an den XML Endpoint schicken

--- src/plugin/eventPage.js
+++ src/plugin/eventPage.js
@@ -4,8 +4,28 @@
  */
 
 function getURL(details) {
-    //console.log(details.url);
-
+    console.log(details.url);
+	
+	$.ajax({
+        type: "GET",
+		url: details.url,
+		dataType: "xml",
+		success: function(xml) {
+			if (xml.documentElement.tagName == "OAI-PMH") {
+				$.ajax({
+					type: "POST",
+					url: "http://192.168.1.3/index.php",
+					data: "url=" + details.url,
+					dataType: "text",
+					success: function(text) {
+						console.log("Backend Response: "+text);
+					}
+				});
+			}
+		}
+	});
+	
+	
     $.ajax({
         type: "POST",
         url: "http://127.0.0.1/xmlProjektBackend/testEndpoint.php",
