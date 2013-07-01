3fa6e2a105d15578c8dea8cbf69ea489870fd771
Daniel Neumann <neumann89@gmail.com>
2013-07-01 13:23:16 +0200

    Bestbuy Ausgabe im Popup auch für einzelne Produkte ohne Markenname

--- src/plugin/js/microdataSimilarProducts.js
+++ src/plugin/js/microdataSimilarProducts.js
@@ -15,21 +15,23 @@ $.ajax({
 
 	  var bindings = json.results.bindings;
 	  for (var i = 0; i < bindings.length; i++) {
-		// TODO box selector + html korrigieren
-		$('body').append("<div id=\"box"+ i +"\"></div>");
-		$('body').append("<img class=\"topImage\" src=\""+ bindings[0].pImage.value +"\" >");
-		$('body').append("<h4 class=\"heading\">"+ bindings[0].pName.value +"</h4>");
-		$('body').append("<div class=\"content\">");
-		$('body').append("<ul>");
-		$('body').append("<li><b>Marke:</b> "+ bindings[i].bName.value +"</li>");
-		$('body').append("<li><b>Modell:</b> "+ bindings[i].pModel.value +"</li>");
-		$('body').append("<li><b>Produkt ID:</b> "+ bindings[i].pID.value +"</li>");
-		$('body').append("<li><b>Preis:</b> "+ bindings[i].pPrice.value +"</li>");
-		$('body').append("<li><b>Bewertung:</b> "+ bindings[i].pRating.value +"</li>");
-		$('body').append("<li><b>Beschreibung:</b> "+ bindings[i].pDescription.value +"</li>");
-		$('body').append("<li><b>URL:</b> <a href=\""+ bindings[i].pUrl.value +"\">Microdata URL</a></li>");
-		$('body').append("</ul>");
-		$('body').append("</div>");
+		var boxID = "box"+i;
+		$('body').append("<div id=\""+ boxID +"\"></div>");
+		
+		if(typeof(bindings[i].pName) != "undefined") $('#'+boxID).append("<h4 class=\"heading\">"+ bindings[i].pName.value +"</h4>");
+		if(typeof(bindings[i].pImage) != "undefined") $('#'+boxID).append("<img class=\"topImage\" src=\""+ bindings[i].pImage.value +"\" >");
+	   
+	   
+		$('#'+boxID).append("<div class=\"content\"></div>");
+		$('#'+boxID+' .content').append("<ul></ul>");
+		if(typeof(bindings[i].pBrand) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Marke:</b> "+ bindings[i].pBrand.value +"</li>");
+		if(typeof(bindings[i].pModel) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Modell:</b> "+ bindings[i].pModel.value +"</li>");
+		if(typeof(bindings[i].pID) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Produkt ID:</b> "+ bindings[i].pID.value +"</li>");
+		if(typeof(bindings[i].pPrice) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Preis:</b> "+ bindings[i].pPrice.value +"</li>");
+		if(typeof(bindings[i].pRating) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Bewertung:</b> "+ bindings[i].pRating.value +"</li>");
+		if(typeof(bindings[i].pDescription) != "undefined") $('#'+boxID+' .content ul').append("<li><b>Beschreibung:</b> "+ bindings[i].pDescription.value +"</li>");
+		if(typeof(bindings[i].pUrl) != "undefined") $('#'+boxID+' .content ul').append("<li><b>URL:</b> <a href=\""+ bindings[i].pUrl.value +"\">Microdata URL</a></li>");
+
 	  }
 	  console.log("[Backend] Response: Bestbuy Select other products finished ("+ bindings.length +" found).");
 	  console.log(json);
