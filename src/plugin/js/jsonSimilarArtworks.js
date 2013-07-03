5fa229e8c478d0489a5ffb25ada2d827b04ef0ec
cone1018 <cone_2@web.de>
2013-07-04 00:32:18 +0200

    parser debugt und traum visualisierung

--- src/plugin/js/jsonSimilarArtworks.js
+++ src/plugin/js/jsonSimilarArtworks.js
@@ -1,41 +1,103 @@
-// http://www.europeana.eu/portal/record/08701/9CDED76786E8F16716ABAE4A7E7AB2A7E57A3726.html?utm_source=api&amp;utm_medium=api&amp;utm_campaign=uhpgWiaD5
+// http://www.europeana.eu/portal/record/08601/97D0837726642A5A66FF551FDE5625CEB4C9F4CC.html?utm_source=api&amp;utm_medium=api&amp;utm_campaign=uhpgWiaD5
 
 var endpoint = "http://127.0.0.1/xmlProjektBackend/sparqlEndpoint.php";
+
+function checkIfImage(img_uri) {
+    var suffix = "IMAGE";
+    return img_uri.indexOf(suffix, img_uri.length - suffix.length) !== -1;
+}
+
+function getImageURI(img_uri) {
+    var parts = img_uri.split("=");
+    var parts = parts[1].split("&");
+    return parts[0];
+}
+
+function getSelectByPublisher(publisher) {
+    var select = "PREFIX ns: <http://purl.org/dc/elements/1.1/> . " +
+	"SELECT ?url ?title ?preview WHERE {" +
+	"?url ns:publisher '" + publisher + "' . " +
+	"?url ns:title ?title . " +
+	"?url ns:preview ?preview . " +
+	"} LIMIT 100";
+    return select;
+}
+
+function getSelectByProvider(provider) {
+    var select = "PREFIX ns: <http://purl.org/dc/elements/1.1/> . " +
+	"SELECT ?url ?title ?preview WHERE {" +
+	"?url ns:provider '" + provider + "' . " +
+	"?url ns:title ?title . " +
+	"?url ns:preview ?preview . " +
+	"} LIMIT 100";
+    return select;
+}
 
 /**
  *  Fragt provider (creator) für diese URL ab und gibt die Bilder (Previews) dafür aus
  */
 function getMyArt(url) {
-    var parts = url.split("?");
-    var parts = parts[0].split(".");
-    var uri = ""
-    for (var j=0; j<(parts.length - 1); j++) {
-	if (j === 0) {
-	    uri += parts[j];
-	    continue;
-	}
-	uri += "." + parts[j];
-    }
-    
-    uri = uri.replace(/portal/, "resolve");
-    console.log(uri);
+    // console.log(url);
     var select = "PREFIX ns: <http://purl.org/dc/elements/1.1/> . " +
-	"SELECT ?title ?creator ?subject WHERE {" + 
-	"<" + uri + "> ns:publisher 'Penn Pub. Co., Philadelphia' ."  +
-	"<" + uri + "> ns:creator ?creator . " +
-	"<" + uri + "> ns:subject ?subject . " +
-	"<" + uri + "> ns:title ?title . " +
+	"SELECT ?publisher ?provider WHERE {" + 
+	"<" + url + "> ns:publisher ?publisher ."  +
+	"<" + url + "> ns:provider ?provider . " +
 	"}  LIMIT 100";
 	
+    // console.log(select);
+    
     $.ajax({
         type: "POST",
         url: endpoint,
         data: {query: select},
         dataType: "text",
 	success: function(text) {
-		    var pre = document.createElement("pre");
-		    pre.innerHTML = text;
-		    document.body.appendChild(pre);
+	    // console.log(text);
+	    var json_res = $.xml2json(text);
+	    // console.log(json_res);
+	    
+	    var publisher = json_res.results.result.binding[0].literal;
+	    var publisher_select = getSelectByPublisher(publisher);
+	    // console.log(publisher_select);
+		    
+	    var provider = json_res.results.result.binding[1].literal;
+	    var provider_select = getSelectByPublisher(provider);
+	    // console.log(provider_select);
+		    
+	    $.ajax({
+		type: "POST",
+		url: endpoint,
+		data: {query: publisher_select},
+		dataType: "text",
+		success: function(text) {
+		    // console.log(text);
+		    json_res = $.xml2json(text);
+		    // console.log(json_res);
+		    
+		    for (var i=0; i<json_res.results.result.length; i++) {
+			var target = json_res.results.result[i].binding[0].uri;
+			// console.log(target);
+			var title = json_res.results.result[i].binding[1].literal;
+			// console.log(title);
+			var img_src = json_res.results.result[i].binding[2].uri;
+			img_src = decodeURIComponent(img_src);
+			// console.log(img);
+			if (checkIfImage(img_src)) {
+			    var link = document.createElement("a");
+			    link.href = target;
+			    link.title = title;
+			    
+			    var img = document.createElement("img");
+			    img.src = getImageURI(img_src);
+			    
+			    link.appendChild(img);
+			    // console.log(link);
+			    document.body.appendChild(link);
+			}
+		    }
 		}
+	    });
+	    
+	}
     });
 }
--- src/plugin/js/jsonToRDF.js
+++ src/plugin/js/jsonToRDF.js
@@ -1,5 +1,5 @@
-// http://europeana.eu/api//v2/search.json?wskey=uhpgWiaD5&query=flower&start=1&rows=12&profile=standard#
-// http://www.europeana.eu/portal/record/08701/9CDED76786E8F16716ABAE4A7E7AB2A7E57A3726.html?utm_source=api&amp;utm_medium=api&amp;utm_campaign=uhpgWiaD5
+// http://europeana.eu/api//v2/search.json?wskey=uhpgWiaD5&query=flowers
+//http://europeana.eu/api//v2/search.json?wskey=uhpgWiaD5&query=Det+Danske+Filminstitut
 
 var API_KEY = "uhpgWiaD5";
 var SPARQL_URL = "http://127.0.0.1/xmlProjektBackend/sparqlEndpoint.php";
@@ -13,100 +13,82 @@ if(document.URL.indexOf("wskey=" + API_KEY) > 0) {
     
     for (var i=0; i<json_object.itemsCount; i++) {
         var item = json_object.items[i];
-        // console.log(item);
+        console.log(item);
         var j;
+        var query_beg = "PREFIX ts: <" + encodeURIComponent("http://purl.org/dc/elements/1.1/") + "> INSERT INTO <" +
+                            encodeURIComponent("http://europeana.eu/api//v2") + "> { <" + encodeURIComponent(item.guid) + ">";
         var query;
         
         // Queries fuer Title
         for (j=0; j<item.title.length; j++) {
-            query = "INSERT INTO <http://europeana.eu/api//v2> { <" +
-                item.guid +
-                "> <http://purl.org/dc/elements/1.1/title> '" +
-                item.title[j] +
-                "'} ";
+            query = " ts:title '" + item.title[j] + "'}";
                 
             $.ajax({
                 type:       "POST",
                 url:        SPARQL_URL,
-                data:       query,
+                data:       "query= " + query_beg + query,
                 dataType:   "text",
                 success:    function(text) {
                     // console.log("Backend Response:\n" + text);
                 }
             });
         }
-        console.log(query);
-        // Queries fuer Provider
+        
+        // Provider
         for (j=0; j<item.provider.length; j++) {
-            query = "INSERT INTO <http://europeana.eu/api//v2> { <" +
-                item.guid +
-                "> <http://purl.org/dc/elements/1.1/publisher> '" +
-                item.provider[j] +
-                "'} ";
-                
-            $.ajax({
-                type:       "POST",
-                url:        SPARQL_URL,
-                data:       query,
-                dataType:   "text",
-                success:    function(text) {
-                    // console.log("Backend Response:\n" + text);
-                }
-            });
-        }
-        console.log(query);
-        // Queries fuer dataProvider
-        for (j=0; j<item.dataProvider.length; j++) {
-            query = "INSERT INTO <http://europeana.eu/api//v2> { <" +
-                item.guid +
-                "> <http://purl.org/dc/elements/1.1/publisher> '" +
-                item.dataProvider[j] +
-                "'} ";
-                
-            $.ajax({
-                type:       "POST",
-                url:        SPARQL_URL,
-                data:       query,
-                dataType:   "text",
-                success:    function(text) {
-                    // console.log("Backend Response:\n" + text);
-                }
-            });
-        }
-        console.log(query);
-        for (j=0; j<item.edmPreview.length; j++) {
-            query = "INSERT INTO <http://europeana.eu/api//v2> { <" +
-                item.guid +
-                "> <http://purl.org/dc/elements/1.1/preview> <" +
-                item.edmPreview[j] +
-                ">} ";
-                
-            $.ajax({
-                type:       "POST",
-                url:        SPARQL_URL,
-                data:       query,
-                dataType:   "text",
-                success:    function(text) {
-                    // console.log("Backend Response:\n" + text);
-                }
-            });
-        }
-        console.log(query);
-        query = "INSERT INTO <http://europeana.eu/api//v2> { <" +
-            item.guid +
-            "> <http://purl.org/dc/elements/1.1/type> '" +
-            item.type +
-            "'}";
+            query = " ts:provider '" + item.provider[j] + "'}";
             
+            $.ajax({
+                type:       "POST",
+                url:        SPARQL_URL,
+                data:       "query= " + query_beg + query,
+                dataType:   "text",
+                success:    function(text) {
+                    // console.log("Backend Response:\n" + text);
+                }
+            });
+        }
+        
+        // dataProvider / Publisher
+        for (j=0; j<item.dataProvider.length; j++) {
+            query = " ts:publisher '" + item.dataProvider[j] + "'}";
+            
+            $.ajax({
+                type:       "POST",
+                url:        SPARQL_URL,
+                data:       "query= " + query_beg + query,
+                dataType:   "text",
+                success:    function(text) {
+                    // console.log("Backend Response:\n" + text);
+                }
+            });
+        }
+        
+        // Preview link
+        for (j=0; j<item.edmPreview.length; j++) {
+            query = " ts:preview <" + encodeURIComponent(item.edmPreview[j]) + ">}";
+            
+            $.ajax({
+                type:       "POST",
+                url:        SPARQL_URL,
+                data:       "query= " + query_beg + query,
+                dataType:   "text",
+                success:    function(text) {
+                    // console.log("Backend Response:\n" + text);
+                }
+            });
+        }
+        
+        // type
+        query = " ts:type '" + item.type + "'}";
         $.ajax({
             type:       "POST",
             url:        SPARQL_URL,
-            data:       query,
+            data:       "query= " + query_beg + query,
             dataType:   "text",
             success:    function(text) {
                 // console.log("Backend Response:\n" + text);
             }
         });
-        console.log(query);
     }
 }
